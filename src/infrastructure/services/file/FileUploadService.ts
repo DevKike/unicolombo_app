2a3367a3c7c50a660fa5b969e8d91199231027c5
DevKike <alvaronarvaez151@gmail.com>
2024-11-07 15:36:58 -0500

    Feat: add create directory uploads if is not existing

--- src/infrastructure/services/file/FileUploadService.ts
+++ src/infrastructure/services/file/FileUploadService.ts
@@ -1,18 +1,24 @@
 import path from "path";
+import * as fs from "fs";
 import { IFileUploadService } from "../../../domain/entities/file/IFIleUploadService";
 import { IFile } from "../../../domain/entities/file/IFile";
-import { FileType } from "../../../domain/types/file/FileType";
+import { FileType } from "../../../domain/enums/file/FileType";
 
 export class FileUploadService implements IFileUploadService {
   private readonly baseUploadPath = path.join(__dirname, "../../uploads");
 
   async uploadFile(file: IFile, fileType: FileType): Promise<string> {
-    // const newFileName = `${file.name}.${Date.now()}`;
+    const directoryPath = path.join(this.baseUploadPath, fileType);
     const filePath = path.join(this.baseUploadPath, fileType, file.name);
+
     try {
+      if (!fs.existsSync(directoryPath)) {
+        fs.mkdirSync(directoryPath, { recursive: true });
+      }
+
       await file.mv(filePath);
+
       const encodedFileName = encodeURIComponent(file.name);
-      
       return `/uploads/${fileType}/${encodedFileName}`;
     } catch (error) {
       throw error;
