44be148adbca0357d06af8c0dec3484f1f6d68c3
DevKike <alvaronarvaez151@gmail.com>
2024-11-07 15:08:27 -0500

    Refactor: update save template form schema

--- src/infrastructure/schemas/templateForm/templateFormSchema.ts
+++ src/infrastructure/schemas/templateForm/templateFormSchema.ts
@@ -1,15 +1,9 @@
 import Joi from "joi";
 
-const code = Joi.number();
-const name = Joi.string();
-const description = Joi.string();
 const filePath = Joi.string();
 const stageId = Joi.number();
 
 export const saveTemplateFormSchema = Joi.object({
-  code: code.required(),
-  name: name.required(),
-  description: description,
   filePath: filePath.required(),
   stage: stageId.required(),
 });
