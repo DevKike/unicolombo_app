e5bd2d483c7ffdaf98ead032285350b0f7c420c0
DevKike <alvaronarvaez151@gmail.com>
2024-11-23 22:35:41 -0500

    Feat: add auth relationship on get method

--- src/infrastructure/services/department/DepartmentService.ts
+++ src/infrastructure/services/department/DepartmentService.ts
@@ -15,7 +15,13 @@ export class DepartmentService implements IDepartmentService {
 
   async getDepartments(): Promise<IDepartment[]> {
     try {
-      return await this.departmentRepository.getAll();
+      const departments = await this.departmentRepository.getAll();
+
+      return departments.map((department) => {
+        const departments = { ...department } as any;
+        delete departments.coordinator.auth.password;
+        return departments;
+      });
     } catch (error) {
       throw error;
     }
