58fe8a6aac03297b20f94d76a2e575f68826befd
DevKike <alvaronarvaez151@gmail.com>
2024-11-25 22:19:59 -0500

    Refactor. change admin to system coordinator

--- src/infrastructure/environment/Environment.ts
+++ src/infrastructure/environment/Environment.ts
@@ -9,8 +9,8 @@ export class Environment {
   static readonly DB_USERNAME = process?.env?.DB_USERNAME;
   static readonly DB_PASSWORD = process?.env?.DB_PASSWORD;
   static readonly DB_NAME = process?.env?.DB_NAME;
-  static readonly AUTH_EMAIL = process?.env.ADMIN_EMAIL  || "admin@unicolombo.edu.co";
-  static readonly AUTH_PASSWORD = process?.env.ADMIN_PASSWORD || "admin123";
+  static readonly AUTH_EMAIL = process?.env.SYSTEM_COORDINATOR_EMAIL  || "admin@unicolombo.edu.co";
+  static readonly AUTH_PASSWORD = process?.env.SYSTEM_COORDINATOR_PASSWORD || "admin123";
   static readonly JWT_SECRET_KEY = process?.env?.JWT_SECRET_KEY || "default_secret";
   static readonly JWT_EXPIRES_IN = process?.env?.JWT_EXPIRES_IN || "24h";
 }
