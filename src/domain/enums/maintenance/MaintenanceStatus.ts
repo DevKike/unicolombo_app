458a4d20843cb0296953c22bb688500514077866
DevKike <alvaronarvaez151@gmail.com>
2024-11-28 21:00:49 -0500

    Feat: refactor maintenance status

--- src/domain/enums/maintenance/MaintenanceStatus.ts
+++ src/domain/enums/maintenance/MaintenanceStatus.ts
@@ -1,9 +1,9 @@
 export enum MaintenanceStatus {
   REQUESTED = "requested",
-  UNDER_EVALUATION = "under_evaluation",
+  UNDER_EVALUATION = "under evaluation",
   APPROVED = "approved",
   ASSIGNED = "assigned",
-  IN_PROGRESS = "in_progress",
+  IN_PROGRESS = "in progress",
   PAUSED = "paused",
   COMPLETED = "completed",
   REJECTED = "rejected",
